3ea059ddc6ba52427175d202e578823170409ebe
Louis Ouellet <louis_ouellet@hotmail.com>
2024-07-25 10:27:04 -0400

    Added view and template for example

---
+++ example/Template/index.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html lang="en" class="h-100 w-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title><?= $this->getLabel() ?></title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+        <?php $this->css(); ?>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <?php $this->js(); ?>
+    </head>
+    <body>
+        <?php require $this->getViewFile(); ?>
+    </body>
+</html>
