0eb8933cf6b702f41c9ad0543e0f13a17578afbe
Louis Ouellet <louis_ouellet@hotmail.com>
2024-07-25 15:13:52 -0400

    Updated the index view file

--- example/View/index.php
+++ example/View/index.php
@@ -1,29 +1,9 @@
 <div class="py-5">
     <div class="container">
-        <h1 class="display-5 fw-lighter">Licenses</h1>
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>License</th>
-                    <th>Product</th>
-                    <th>User</th>
-                    <th>Term</th>
-                    <th>Duration</th>
-                    <th>Expire</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($this->Return as $license): ?>
-                    <tr>
-                        <td><?= $license['license'] ?></td>
-                        <td><?= $license['product'] ?></td>
-                        <td><?= $license['user'] ?></td>
-                        <td><?= $license['term'] ?></td>
-                        <td><?= $license['duration'] ?></td>
-                        <td><?= $license['expire'] ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+        <h1 class="display-5 fw-lighter">Software Licensing System</h1>
+        <div class="w-100 btn-group m-3">
+            <a href="/licenses" class="btn btn-light">Licenses</a>
+            <a href="/products" class="btn btn-light">Products</a>
+        </div>
     </div>
 </div>
