564195d5e160793858581c1d65f3d16584ae2da2
Louis Ouellet <louis_ouellet@hotmail.com>
2024-07-29 08:37:46 -0400

    Prettyfied the index view

--- example/View/index.php
+++ example/View/index.php
@@ -1,9 +1,27 @@
 <div class="py-5">
     <div class="container">
-        <h1 class="display-5 fw-lighter">Software Licensing System</h1>
-        <div class="w-100 btn-group m-3">
-            <a href="/licenses" class="btn btn-light">Licenses</a>
-            <a href="/products" class="btn btn-light">Products</a>
+        <a href="<?= $this->getRoute() ?>" class="nav-link px-2 mb-5">
+            <h1 class="display-5 fw-lighter m-0">
+                Software Licensing System <span style="font-size: 0.5em;">Home</span>
+            </h1>
+        </a>
+        <div class="row g-3">
+            <div class="col">
+                <a href="/licenses" class="d-block btn btn-outline-primary rounded-5 border-2 p-0 text-decoration-none">
+                    <div class="p-5 d-flex flex-column justify-content-center align-items-center">
+                        <i class="bi bi-key display-1"></i>
+                        <h4 class="fw-lighter">Licenses</h4>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="/products" class="d-block btn btn-outline-primary rounded-5 border-2 p-0 text-decoration-none">
+                    <div class="p-5 d-flex flex-column justify-content-center align-items-center">
+                        <i class="bi bi-box2 display-1"></i>
+                        <h4 class="fw-lighter">Products</h4>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
